766ef8f86459a5922b196ecb6ecf9b60910fa4d9
TomekPetrykowski <tomasz.petrykowski.20@gmail.com>
2023-11-10 19:16:21 +0100

    Added control to game

--- game.py
+++ game.py
@@ -10,10 +10,15 @@ class AlienInvasion:
     def __init__(self):
         pygame.init()
 
-        self.set = Settings()
+        self.settings = Settings()
+
+        # fullscreen - to use later
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
 
         self.screen = pygame.display.set_mode(
-            (self.set.screen_width, self.set.screen_height))
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Inwazja obcych")
 
         self.ship = Ship(self)
@@ -21,6 +26,7 @@ class AlienInvasion:
     def run_game(self):
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -28,8 +34,28 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
 
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        if event.key == pygame.K_q:
+            sys.exit()
+
     def _update_screen(self):
-        self.screen.fill(self.set.bg_color)
+        self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
 
         pygame.display.flip()
